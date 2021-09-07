4b2d6bb69690ccdf0f67150adb4607057bd32a0c
Eric Cheng <68196762+import-brain@users.noreply.github.com>
2021-09-07 10:03:27 -0400

    added logic, added CONTRIBUTORS

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,5 @@
+# Credits
+
+## Libraries
+
+Thank you to the CS50 team at Harvard for creating the CS50 library, which was used in this project!
---
+++ paycheckCalc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    
+    float hourlyPay = get_float("How much do you make per hour? ");
+    
+    int totalHours = get_int("How many hours did you work this week? ");
+    
+    int overtimeHours;
+
+    int normalHours;
+
+    if (totalHours > 40)
+    {
+        
+        normalHours = 40;
+
+        overtimeHours = totalHours - normalHours;
+
+    }
+    else if (totalHours < 40)
+    {
+        normalHours = totalHours;
+        overtimeHours = 0;   
+    }
+
+    float totalPay = (normalHours * hourlyPay) + (overtimeHours * hourlyPay * 1.5);
+
+    printf("Your total pay is: %f\n", totalPay);
+}
